e92641547efa8fabd149f4007cb5e0a5039a1cf3
Adam Nelson <anelson@apocryph.org>
2018-06-16 19:04:51 -0400

    Rough out the data structures and some initial tests

---
+++ README.md
@@ -0,0 +1,28 @@
+# Notes from the CQF code
+
+## Key terms
+
+* `n` - The number of expected insertions.  Note this isn't unique values; it's all insertion operations
+* `δ` - The false positive rate as a fraction like `1/64` or `1/512`
+* `p` - The number of bits from the hash function which is used to actually look up values.  This is computed based on
+  the desired false positive rate `δ` and the insertion count `n` thusly:
+
+  `p = log2(n/δ)`
+* `q` - The number of bits of `p` which are used as the quotient to determine the home slot where a given hash value
+  should go
+* `r` - The bits of `p` left over after the `q` bits have been used to find the home slot.  This remainder is used to
+  distinguish between multiple values that have the same quotient.  Because of the probabilistic nature of this data
+  structure, the worst case is that two hashes `h(x)` and `h(y)` are disambiguated entirely based on the low `r` bits of
+  their hashes, and thus the worst case false positive rate is `2^-r`.
+
+  So for `r = 6`, the false positive rate is `1/(2^6)` which is `1/64`
+* `BITS_PER_SLOT` - In the code sometimes this is used for `r`.  The code uses conditional compilation to choose either
+  compile-time or run-time implementation.  For values `8`, `16`, `32`, and `64` there are accelerated implementations
+  that use the corresponding integer types.
+
+# References
+
+* [Go implementation that's a bit more readable](https://github.com/nfisher/rsqf)
+* [Morning Paper explainer for the actual
+  paper](https://blog.acolyer.org/2017/08/08/a-general-purpose-counting-filter-making-every-bit-count/)
+* [The actual paper](https://www3.cs.stonybrook.edu/~ppandey/files/p775-pandey.pdf)
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-const BITS_PER_SLOT: usize = 8;
+use std::vec::Vec;
+
+const BITS_PER_SLOT: usize = 9; //corresponds to false positive rate of 1/512
 const BLOCK_OFFSET_BITS: usize = (6); //6 seems fastest
 const SLOTS_PER_BLOCK: usize = (1 << BLOCK_OFFSET_BITS);
 const METADATA_WORDS_PER_BLOCK: usize = ((SLOTS_PER_BLOCK + 63) / 64);
@@ -7,14 +9,86 @@ struct Block {
     offset: u8,
     occupieds: [u64; METADATA_WORDS_PER_BLOCK],
     runends: [u64; METADATA_WORDS_PER_BLOCK],
-    slots: [u8; SLOTS_PER_BLOCK],
+    slots: [u64; BITS_PER_SLOT],
 }
 
-pub struct CQF {}
+struct Metadata {
+    n: usize,
+    q: u8,
+    r: u8,
+    nblocks: usize,
+    nelements: usize,
+    ndistinct_elements: usize,
+    nslots: usize,
+    noccupied_slots: usize,
+}
+
+pub struct CQF {
+    meta: Metadata,
+    blocks: Vec<Block>,
+}
+
+impl CQF {
+    pub fn new(n: usize, r: u8) -> CQF {
+        assert!(SLOTS_PER_BLOCK == 64usize); //this code assumes 64 slots per block always
+        assert!(r as usize == BITS_PER_SLOT); //TODO: figure out how to make this configurable
+        let meta = CQF::calculate_metadata(n, r);
+        let blocks = Vec::with_capacity(meta.nblocks);
+        return CQF { meta, blocks };
+    }
+
+    /// Given the insert count `n` and the remainder bits `r`, computes the metadata
+    /// for a filter which will have worst-case false positive rate of `2^-r`
+    fn calculate_metadata(n: usize, r: u8) -> Metadata {
+        assert!(r > 1);
+        assert!(n > 0);
+
+        let sigma = 2.0f64.powi(-(r as i32));
+        let p = ((n as f64) / sigma).log2().ceil() as u8;
+
+        assert!(p > r);
+
+        let q = p - r;
+
+        let total_slots = 1usize << q; //2^q slots in the filter
+        let nblocks = (total_slots + SLOTS_PER_BLOCK - 1) / SLOTS_PER_BLOCK;
+
+        return Metadata {
+            n,
+            r,
+            q,
+            nblocks,
+            nelements: 0,
+            ndistinct_elements: 0,
+            nslots: total_slots,
+            noccupied_slots: 0,
+        };
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::CQF;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn creates_empty_filter() {
+        let filter = CQF::new(10000, 9);
+    }
+
+    #[test]
+    #[should_panic]
+    fn panics_on_invalid_r() {
+        CQF::new(10000, 8);
+    }
+
+    #[test]
+    fn computes_valid_metadata() {
+        let filter = CQF::new(10000, 9);
+
+        assert_eq!(filter.meta.n, 10000);
+        assert_eq!(filter.meta.r, 9);
+        assert_eq!(filter.meta.q, 14);
+        assert_eq!(filter.meta.nslots, 1usize << 14);
+        assert_eq!(filter.meta.nblocks, (filter.meta.nslots + 64 - 1) / 64);
     }
 }
