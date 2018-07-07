ea4d9ce4d537c98e977b7458ed8b67f38f6e9a71
Adam Nelson <anelson@apocryph.org>
2018-07-07 19:46:53 -0400

    Implement find_slot_range and skeleton of shift_left_slot in bitarray

--- src/bitarray.rs
+++ src/bitarray.rs
@@ -88,6 +88,9 @@ impl SingleBitArray for u64 {
 /// 14, but in theory it can be from 2 up to 63 (though at both extremes the structure becomes
 /// practically useless).
 pub trait MultiBitArray {
+    #[inline]
+    fn get_total_slots(&self, nbits: usize) -> usize;
+
     /// Gets the `nbits`-bit slot at `index` (`index` is the slot index, not the bit index)
     #[inline]
     fn get_slot(&self, nbits: usize, index: usize) -> u64;
@@ -99,6 +102,47 @@ pub trait MultiBitArray {
     ///
     /// Returns the previous value at index `index`.
     fn set_slot(&mut self, nbits: usize, index: usize, val: u64) -> u64;
+
+    /// Shifts some or all of the slots right by one slot.  This obviously could shift one slot off
+    /// the end of the array.
+    ///
+    /// # Arguments
+    ///
+    /// `nbits` - The size of each slot in bits
+    /// `start_index` - The index of the slot in this block where the shift operation
+    /// should start.  `0` starts the shift from the start of the block.
+    /// `slot_count` - The number of slots to shift.  Note that this can be more than the number of
+    /// slots in a block; if so, `shift_slots` will shift from `start_index` to the end of
+    /// the block, and include in its return value the `slot_count` minus the number of slots it
+    /// shifted already; this lets the caller call this method in a loop iteratively shifting all
+    /// of the slots.
+    /// `insert_value` - If a previous block also shifted its slots, this is the value of
+    /// the slot shifted off the end of that block, which will be placed into this block at
+    /// `start_index` after its contents are shifted up one.  Set to `0` for the block
+    /// where the shifting operation starts.
+    ///
+    /// # Returns
+    ///
+    /// Returns a tuple which makes it easy to perform the shift iteratively.
+    ///
+    /// `(remaining_slot_count, top_slot)`:
+    ///
+    /// * `remaining_slot_count` - How many slots are still to be shifted, after this block's shift
+    /// operation has completed.  A convenience value; it's `slot_count` minus the number of slots
+    /// effected by this shift operation.
+    /// * `top_slot` - The value in the highest-most slot in this shift region, which due to the shift has
+    /// overflowed the region and should be placed into the next array if the shift operation is
+    /// going to continue.  This can be pased back in to `shift_slots_left` as the
+    /// `insert_value` argument to ensure it's placed in the next array in the right
+    /// place.
+    #[inline]
+    fn shift_slots_left(
+        &mut self,
+        nbits: usize,
+        start_index: usize,
+        slot_count: usize,
+        insert_value: u64,
+    ) -> (usize, u64);
 
     /// Inserts the `nbits`-bit slot at `index` (`index` is the slot index, not the bit index)
     ///
@@ -125,6 +169,10 @@ pub trait MultiBitArray {
 /// introduces an excessive performance penalty then we'll have to use macros and generate
 /// implementations for various possible sizes of `u64` arrays.  
 impl MultiBitArray for [u64] {
+    fn get_total_slots(&self, nbits: usize) -> usize {
+        self.len() * 64 / nbits
+    }
+
     #[inline]
     fn get_slot(&self, nbits: usize, index: usize) -> u64 {
         let (slot_word_index, slot_bit_offset_in_word, bits_in_first_word, bits_in_second_word) =
@@ -202,6 +250,98 @@ impl MultiBitArray for [u64] {
     }
 
     #[inline]
+    fn shift_slots_left(
+        &mut self,
+        nbits: usize,
+        start_index: usize,
+        slot_count: usize,
+        insert_value: u64,
+    ) -> (usize, u64) {
+        debug_assert!(start_index < nbits);
+        debug_assert!(slot_count > 0);
+        debug_assert!((insert_value & !bitmask!(nbits)) == 0);
+        /*
+         *        let mut shifted_slot: u64;
+         *
+         *        //Depdning upon the start_index and slot_count, this shift operation can include
+         *        //one or more of the following variations:
+         *        //
+         *        /[> shift bits within a single word, starting from the middle of the word up to the end
+         *        /[> shift bits within a single word, starting from 0 and up to a place in the middle of the
+         *        //word
+         *        /[> shift bits within a single word entirely within the middle of the word
+         *        /[> shift bits across the entirety of a whole word
+         *
+         *        //Figure out where the first and last slot to be shifted are.
+         *        let (
+         *            start_word_index,
+         *            start_bit_offset_in_word,
+         *            start_bits_in_first_word,
+         *            start_bits_in_second_word,
+         *        ) = find_slot(self, nbits, start_index);
+         *
+         *        //Might need to clamp the end slot if slot_count exceeds the slots in this array
+         *        let end_index = if start_index + slot_count > self.get_total_slots(nbits) {
+         *            self.get_total_slots(nbits) - 1
+         *        } else {
+         *            start_index + slot_count - 1
+         *        };
+         *
+         *        let (
+         *            end_word_index,
+         *            end_bit_offset_in_word,
+         *            end_bits_in_first_word,
+         *            end_bits_in_second_word,
+         *        ) = find_slot(self, nbits, end_index);
+         *
+         *        if start_index == 0 && slot_count >= nbits {
+         *            //This is the most efficient path.  The entire contents of this block will be shifted
+         *            //by `nbits` bits.  Work through the words from end to start
+         *            let mut shifted_slot: u64 = insert_value;
+         *
+         *            for word_index in 0..BITS_PER_SLOT {
+         *                let word = self.slots[word_index];
+         *                let new_word = word << nbits | shifted_slot;
+         *                shifted_slot = word >> (64 - nbits);
+         *
+         *                self.slots[word_index] = new_word;
+         *            }
+         *
+         *            // That's it; shifted the entire block left by nbits; shifted_slot contains the value
+         *            // of the top-most slot in the block which should be shifted into the bottom of the
+         *            // next block.
+         *            (slot_count - nbits, shifted_slot)
+         *        } else if start_index == 0 {
+         *            //Starting at the beginning of the block but the end is also in this block.
+         *            //Figure out how many entire words are covered, and if there's part of a word at the
+         *            //end handle that separately;
+         *            let whole_words = slot_count * BITS_PER_SLOT / 64;
+         *
+         *            let mut shifted_slot: u64 = insert_value;
+         *
+         *            for word_index in 0..whole_words {
+         *                let word = self.slots[word_index];
+         *                let new_word = word << nbits | shifted_slot;
+         *                shifted_slot = word >> (64 - nbits);
+         *
+         *                self.slots[word_index] = new_word;
+         *            }
+         *
+         *            //For any remaining slots after the whole words just do the shift a slot at a time.
+         *            let remaining_bits = (slot_count * BITS_PER_SLOT) % 64;
+         *            let final_word = whole_words;
+         *
+         *            let word = self.slots[final_word];
+         *
+         *            (0, 0) //TODO: This is just to get the code to compile it's not right
+         *        } else {
+         *            panic!("NYI");
+         *        }
+         */
+        panic!("NYI");
+    }
+
+    #[inline]
     #[allow(unused_variables)]
     #[allow(dead_code)]
     fn insert_slot(&mut self, nbits: usize, index: usize, val: u64) -> Option<u64> {
@@ -236,7 +376,7 @@ impl MultiBitArray for [u64] {
 fn find_slot(arr: &[u64], nbits: usize, index: usize) -> (usize, usize, usize, usize) {
     debug_assert!(nbits > 0);
 
-    let total_slots = arr.len() * 64 / nbits;
+    let total_slots = arr.get_total_slots(nbits);
     let block_word_size = mem::size_of::<u64>() * 8;
     assert!(index < total_slots);
 
@@ -268,6 +408,82 @@ fn find_slot(arr: &[u64], nbits: usize, index: usize) -> (usize, usize, usize, u
             bits_in_second_word,
         )
     }
+}
+
+/// Similar to `find_slot`, but instead of looking for a specific slot it looks for a range of
+/// slots and computes their start and end boundaries.  This is useful when performing shift
+/// operations
+///
+/// Given a index of a slot and the `nbits` number of bits per slot and a `count` of slots, finds the
+/// location of the start and end of the range of bits in the array.
+///
+/// # Returns
+///
+/// `(start_word_index, bits_in_start_word, end_word_index, bits_in_end_word)`
+///
+/// * `start_word_index` - The index of the word that contains the first bit of slot `index`
+/// * `bits_in_start_word` - How many bits of `start_word_index` contain this range.  If the range
+/// spans multiple words, this will be the upper bits of that word.  
+/// * `end_word_index` - The index of the word that contains the last bit of slot `index+count-1`.
+/// If the range does not span multiple words, this is the same as `start_word_index`.
+/// * `bits_in_end_word` - The number of bits in `end_word_index` that contain this range.  If the
+/// range spans multiple words, these will be the lower bits of that word.
+#[inline]
+#[allow(dead_code)] // Just for now until higher level modules call this
+fn find_slot_range(
+    arr: &[u64],
+    nbits: usize,
+    start_index: usize,
+    count: usize,
+) -> (usize, usize, usize, usize) {
+    debug_assert!(nbits > 0);
+
+    let total_slots = arr.get_total_slots(nbits);
+    let block_word_size = mem::size_of::<u64>() * 8;
+    assert!(start_index + count <= total_slots);
+
+    //Figure out where the first and last slot to be shifted are.
+    let end_index = start_index + count - 1;
+    let (start_word_index, start_bit_offset_in_word, _, _) = find_slot(arr, nbits, start_index);
+
+    let (end_word_index, end_bit_offset_in_word, end_bits_in_first_word, end_bits_in_second_word) =
+        find_slot(arr, nbits, end_index);
+
+    //how many bits, total, are in the start word?
+    if start_word_index == end_word_index && end_bits_in_second_word == 0 {
+        let total_bits = count * nbits;
+
+        // in debug builds sanity-check this total; this expression and the one used for total_bits
+        // should be equivalent
+        debug_assert!(
+            total_bits
+                == end_bit_offset_in_word + end_bits_in_first_word - start_bit_offset_in_word
+        );
+
+        return (start_word_index, total_bits, end_word_index, total_bits);
+    }
+
+    //start and end words are not the same so this spans multiple words.
+    let bits_in_start_word = block_word_size - start_bit_offset_in_word;
+
+    //end_word_index is the index of the word where the last slot STARTS, not neceessarily where
+    //all of its bits are.  If the `end_bits_in_second_word` value is non-zero, it means this last
+    //slot spills over into a word AFTER `end_word_index`
+    let (end_word_index, bits_in_end_word) = if end_bits_in_second_word == 0 {
+        (
+            end_word_index,
+            end_bit_offset_in_word + end_bits_in_first_word,
+        )
+    } else {
+        (end_word_index + 1, end_bits_in_second_word)
+    };
+
+    (
+        start_word_index,
+        bits_in_start_word,
+        end_word_index,
+        bits_in_end_word,
+    )
 }
 
 #[cfg(test)]
@@ -353,6 +569,21 @@ mod tests {
         let mut expected_bits_in_second_word = 0usize;
         let block = [0u64; WORDS_PER_BLOCK];
 
+        //Start with some easy ones; the first and the last slot in the array
+        let (slot_word_index, slot_bit_offset_in_word, bits_in_first_word, bits_in_second_word) =
+            find_slot(&block, BITS_PER_SLOT, 0);
+        assert_eq!(0, slot_word_index);
+        assert_eq!(0, slot_bit_offset_in_word);
+        assert_eq!(BITS_PER_SLOT, bits_in_first_word);
+        assert_eq!(0, bits_in_second_word);
+
+        let (slot_word_index, slot_bit_offset_in_word, bits_in_first_word, bits_in_second_word) =
+            find_slot(&block, BITS_PER_SLOT, SLOTS_PER_TEST_BLOCK - 1);
+        assert_eq!(WORDS_PER_BLOCK - 1, slot_word_index);
+        assert_eq!(64 - BITS_PER_SLOT, slot_bit_offset_in_word);
+        assert_eq!(BITS_PER_SLOT, bits_in_first_word);
+        assert_eq!(0, bits_in_second_word);
+
         for i in 0..SLOTS_PER_TEST_BLOCK {
             let (slot_word_index, slot_bit_offset_in_word, bits_in_first_word, bits_in_second_word) =
                 find_slot(&block, BITS_PER_SLOT, i);
@@ -390,5 +621,41 @@ mod tests {
                 expected_bits_in_second_word = bits_per_slot - expected_bits_in_first_word;
             }
         }
+    }
+
+    #[test]
+    pub fn find_slot_range_tests() {
+        //let bits_per_slot = BITS_PER_SLOT;
+        let block = [0u64; WORDS_PER_BLOCK];
+
+        //As a first category of test inputs, find_slot_range with a count of 1 should always
+        //produce equivalent results to find_slot
+        for i in 0..SLOTS_PER_TEST_BLOCK {
+            let (slot_word_index, _, bits_in_first_word, bits_in_second_word) =
+                find_slot(&block, BITS_PER_SLOT, i);
+
+            let (start_word_index, bits_in_start_word, end_word_index, bits_in_end_word) =
+                find_slot_range(&block, BITS_PER_SLOT, i, 1);
+
+            assert_eq!(slot_word_index, start_word_index, "slot {} count 1", i);
+            assert_eq!(bits_in_first_word, bits_in_start_word, "slot {} count 1", i);
+
+            if bits_in_second_word > 0 {
+                assert_eq!(slot_word_index + 1, end_word_index, "slot {} count 1", i);
+                assert_eq!(bits_in_second_word, bits_in_end_word, "slot {} count 1", i);
+            } else {
+                assert_eq!(slot_word_index, end_word_index, "slot {} count 1", i);
+                assert_eq!(bits_in_first_word, bits_in_end_word, "slot {} count 1", i);
+            }
+        }
+
+        //Try the entire array make sure it computes that properly
+        let (start_word_index, bits_in_start_word, end_word_index, bits_in_end_word) =
+            find_slot_range(&block, BITS_PER_SLOT, 0, SLOTS_PER_TEST_BLOCK);
+
+        assert_eq!(0, start_word_index);
+        assert_eq!(64, bits_in_start_word);
+        assert_eq!(block.len() - 1, end_word_index);
+        assert_eq!(64, bits_in_end_word);
     }
 }
