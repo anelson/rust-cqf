6782c7031dd848cab2159ed8f5b99d1fae198d43
Adam Nelson <anelson@apocryph.org>
2018-07-12 09:57:21 -0400

    Implement insert with shift and some tests
    Tests are failing now there's some bug there.

--- src/logical.rs
+++ src/logical.rs
@@ -1,5 +1,6 @@
 //! Implements the logical RSQF operations in a layer one removed from the bit-fiddly details in
 //! the Physical layer.
+
 use physical;
 
 #[allow(dead_code)] // for now
@@ -123,9 +124,66 @@ impl LogicalData {
         let empty_slot_index =
             empty_slot_index.expect("the underlying data structure has no free slots left!");
 
-        // self.physical.shift_slots(insert_index, empty_slot_index);
-        // TODO: Implement this now
-        panic!("NYI");
+        //Shift all of the remainder values and runends between insert_index and empty_slot_index
+        //to the left by one.  That will overwrite the contents at empty_slot_index (which is emtpy
+        //so that's fine), and make room at insert_index for this remainder and runend bit
+        self.physical
+            .shift_slots_left(insert_index, empty_slot_index, remainder);
+        self.physical
+            .shift_runends_left(insert_index, empty_slot_index - 1, true);
+
+        //The new runend is at `insert_index`, which is one slot after the old runend at
+        //`runend_index`.  Clear that old runend bit
+        self.physical.set_runend(runend_index, false)
+
+        //TODO: Update the block offsets
+    }
+
+    /// Performs a comprehensive (and slow!) scan of the internal structure verifying all
+    /// invariants and checking the integrity of the structure.  
+    ///
+    /// # Panics
+    ///
+    /// Panics if there is any validation error with the content of the structure
+    #[cfg(test)] //This is used in the tests only
+    pub fn validate_contents(&self) -> () {
+        assert!(self.qbits > 1);
+        assert!(self.rbits >= 3);
+
+        //There should be the saself.number of runends as occupieds in total
+        let total_runends = self.physical.count_runends(0, self.physical.len());
+        let total_occupieds = self.physical.count_occupieds(0, self.physical.len());
+
+        assert_eq!(
+            total_runends, total_occupieds,
+            "the runend and occupied bit counts don't match"
+        );
+
+        //The first occupied bit should be at or before the position of the first runend
+        for i in 0..self.physical.len() {
+            if self.physical.is_occupied(i) {
+                break;
+            } else {
+                assert!(
+                    !self.physical.is_runend(i),
+                    "the first set runend bit is in slot {} before the first occupied bit!",
+                    i
+                );
+            }
+        }
+
+        //The last runend bit should be at or after the position of the last occupied
+        for i in (0..self.physical.len()).rev() {
+            if self.physical.is_runend(i) {
+                break;
+            } else {
+                assert!(
+                    !self.physical.is_occupied(i),
+                    "the last set occupied bit is in slot {} before the last runend bit!",
+                    i
+                );
+            }
+        }
     }
 
     /// In debug builds, ensures quotient values are masked out properly and only contain the
@@ -196,6 +254,83 @@ mod logicaldata_tests {
 
             //But it does NOT contain any other remainder for this quotient
             assert_eq!(false, ld.contains(quotient, remainder ^ 0xff));
+        }
+    }
+
+    /// Test inserts for multiple remainders in one quotient requiring shifting of values
+    #[test]
+    pub fn insert_with_shifting_test() {
+        // Test cases to exercise this scenarios
+        // Each test case is a vector of quotients and their remainders.  The quotients and their
+        // remainders are inserted in order, and after every operation the invariants of the
+        // structure are verified.
+        let test_cases: Vec<Vec<(u64, Vec<u64>)>> = vec![
+            // Just insert a single quotient and remainder; simply and inoffensive
+            vec![(100, vec![1])],
+            // Insert two quotients each with a remainder, right next to each other
+            vec![(100, vec![1]), (101, vec![2])],
+            // Insert one quotient with two remainders
+            vec![(100, vec![1, 2])],
+            // Insert a quotient with two remainders then another quotient in the next slot
+            vec![(100, vec![1, 2]), (101, vec![3])],
+        ];
+
+        for (index, ref test) in test_cases.iter().enumerate() {
+            let mut ld = LogicalData::new(TEST_QBITS, TEST_RBITS);
+
+            for &(ref quotient, ref remainders) in *test {
+                let context = format!(
+                    "test index={} quotient={} remainders={:?}",
+                    index, *quotient, *remainders
+                );
+
+                ld.validate_contents();
+
+                assert_eq!(
+                    false,
+                    ld.physical.is_occupied(*quotient as usize),
+                    "context=[{}]",
+                    context
+                );
+
+                for r in remainders {
+                    assert_eq!(
+                        false,
+                        ld.contains(*quotient, *r),
+                        "context=[r={} {}]",
+                        *r,
+                        context
+                    );
+                    ld.insert(*quotient, *r);
+                    ld.validate_contents();
+                    assert_eq!(
+                        true,
+                        ld.contains(*quotient, *r),
+                        "context=[r={} {}]",
+                        *r,
+                        context
+                    );
+                }
+            }
+
+            //Now all the inserts are done; make sure that hasn't broken anything
+            for &(ref quotient, ref remainders) in *test {
+                let context = format!(
+                    "test index={} quotient={} remainders={:?}",
+                    index, *quotient, *remainders
+                );
+
+                assert_eq!(
+                    true,
+                    ld.physical.is_occupied(*quotient as usize),
+                    "context=[{}]",
+                    context
+                );
+
+                for r in remainders {
+                    assert_eq!(true, ld.contains(*quotient, *r), "context=[{}]", context);
+                }
+            }
         }
     }
 }
